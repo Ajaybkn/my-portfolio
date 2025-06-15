bb111e217a79c0b3ba1d75c188c1289c536dd0cd
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2025-06-16 01:30:35 +0530

    update

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "export",
+	images: {
+		unoptimized: true, // disables optimization which breaks on Netlify static export
+	},
 	trailingSlash: true, // optional but avoids routing issues on Netlify
 };
 
