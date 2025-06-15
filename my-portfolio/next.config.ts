3dc35febefa6b6588ae801ee52add958af4f6488
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2025-06-15 19:53:16 +0530

    update

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: "export",
+	trailingSlash: true, // optional but avoids routing issues on Netlify
 };
 
 export default nextConfig;
