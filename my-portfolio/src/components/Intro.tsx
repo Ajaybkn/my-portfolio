82808245110f8a258cc2d52b25d2a4de42cd2160
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2025-07-19 11:40:59 +0530

    update

--- my-portfolio/src/components/Intro.tsx
+++ my-portfolio/src/components/Intro.tsx
@@ -84,7 +84,7 @@ const buttonVariants = {
 
 const Intro = () => {
 	const [isMounted, setIsMounted] = useState(false);
-	const name = "Ajay S. Khangarot";
+	const name = "Ajay Singh Khangharot";
 	const role = "Full Stack Developer";
 	const description = "Crafting digital experiences with modern technologies";
 
